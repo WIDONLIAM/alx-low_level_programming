76fa25d460f05446bf929609fc1c6c33c5c4277c
widonliam <wildonliam47@gmail.com>
2023-07-13 15:24:13 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,5 +3,5 @@
 int main(){
 
       char Hello puts[30]= "Programming is like building a multilingual puzzle";
-      puts (Hello puts); // prints to stdout
-      return (0)
+      puts ("Programming is like building a multilingual puzzle")
+      return (0);
