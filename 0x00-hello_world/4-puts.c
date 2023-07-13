ec1267304aeaf80ae7856b2fc482d7b8a4ccc81a
widonliam <wildonliam47@gmail.com>
2023-07-13 15:32:42 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main -entry point
+ *
+ * Return: Always (0)
+ */
 int main(){
 
-      char Hello puts[50]= "Programming is like building a multilingual puzzle\n";
+      char Hello puts[50]= "Programming is like building a multilingual puzzle";
       puts ("Programming is like building a multilingual puzzle")
       return (0);
