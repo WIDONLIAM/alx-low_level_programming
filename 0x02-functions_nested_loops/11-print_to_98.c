948dc6e2484828fd79665464e10f5e8e24b8073e
widonliam <wildonliam47@gmail.com>
2023-07-19 17:37:51 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
 /**
  * print_to_98 - prints all natural numbers from n to 98
  *
- * @n: starting point
+ a* @n: starting point
  *
  * Return: returns nothing
  *
@@ -14,11 +14,11 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (n = n; n <= 97; n++)
-			printf("%d, ", n);
+	for (n = n; n <= 97; n++)
+		printf("%d, ", n);
 	}
 	else
-		for (n = n; n > 98; n--)
-			printf("%d, ", n);
-			printf("98\n");
+	for (n = n; n > 98; n--)
+		printf("%d, ", n);
+	printf("98\n");
 }
