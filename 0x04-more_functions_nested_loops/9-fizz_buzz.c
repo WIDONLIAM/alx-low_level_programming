5fbd906f2d3fe6b7f40e127b75cd5caac4d0d425
widonliam <wildonliam47@gmail.com>
2023-07-21 07:26:45 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,7 +3,7 @@
 
 /**
  * main - prints the numbers from 1 to 100, followed by a new line
- * But for multiples of three prints Fizz instead of the number
+ * but for multiples of three prints Fizz instead of the number
  * and for the multiples of five prints Buzz
  * Return: Always 0 (success)
  */
