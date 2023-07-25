3a2be434110d20224bd6b9d7c836aa5fde88506f
widonliam <wildonliam47@gmail.com>
2023-07-24 18:41:14 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,17 +12,17 @@ char *_ strcpy(char *dest, char *src)
 	int l = 0;
 	int x = 0;
 
-		while (*(src + l) != '\0')
-		{
-			l++;
-		}
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
 
-		for (; x < l; x++)
-		{
-			dest[x] = src[x];
-		}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
 
-			dest[l] = '\0';
+		dest[l] = '\0';
 
-			return (dest);
+		return (dest);
 }
