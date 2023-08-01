00929232cc9341a202f7985b86d43afdef0ee586
widonliam <wildonliam47@gmail.com>
2023-07-31 19:31:40 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,16 +15,16 @@ char *_strstr(char *haystack, char *needle)
 		char *l = haystack;
 		char *p = needle;
 
-		while (*l = *p && *p != '\0')
-		{
-			l++;
-			p++;
-		}
+			while (*l = *p && *p != '\0')
+			{
+				l++;
+				p++;
+			}
 
 
-		if (*p == '\0')
+			if (*p == '\0')
 
-		return (haystack);
+				return (haystack);
 	}
 
 	return (0);
