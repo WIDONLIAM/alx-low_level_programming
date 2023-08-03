084a4633c3c761e8db9bc4618ed0b2139710f11b
widonliam <wildonliam47@gmail.com>
2023-08-02 17:27:14 -0700

    _putchar.c

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unist.h>
+
+/**
+ *
+ * _putchar - writes the c character to (stdout)
+ * @c: shows the character to print
+ *
+ * Return: success (1)
+ *
+ */
+
+int _putcgar(char c)
+{
+	return (write(1, &c, 1));
+
+}
