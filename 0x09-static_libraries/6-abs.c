d8f56cd5818ae0c6631f40bc81defd2d0314ec77
widonliam <wildonliam47@gmail.com>
2023-08-05 19:10:32 -0700

    6-abs.c

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Will be computeing the  positive value of an integer
+ * @n: Is the integer to check
+ *
+ * Return: positive value of the integer
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+
+	return (-n);
+}
