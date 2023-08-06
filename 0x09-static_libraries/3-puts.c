5e2c39de89b91959809dc934ecf8ae19a8b7f6c9
widonliam <wildonliam47@gmail.com>
2023-08-05 18:16:00 -0700

    3-puts.c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+void _puts(char *str)
+#include<stdio.h>
+
+/**
+ * main - Will be the entry point
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
