59080fecc4f75e2ec6f81856a02445966ba720d6
widonliam <wildonliam47@gmail.com>
2023-08-08 16:23:12 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,14 +15,14 @@ char *_strdup(char *str)
 	int i = 0;
 	int r = 0;
 
-		aaa = malloc(sizeof(char) * (i + 1));
-
 		if (str == NULL)
 
 		return (NULL);
 
 		while (str[i] != '\0')
 			i++;
+
+		aaa = malloc(sizeof(char) * (i + 1));
 
 		if (aaa == NULL)
 
