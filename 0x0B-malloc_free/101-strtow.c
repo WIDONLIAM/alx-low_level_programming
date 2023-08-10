de888f6055617bfde7f6edba60ea47500f8c491b
widonliam <wildonliam47@gmail.com>
2023-08-09 19:28:10 -0700

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,21 +12,21 @@ int count_word(char *s)
 {
 	int flag, c, w;
 
-		flag = 0;
-		w = 0;
+	flag = 0;
+	w = 0;
 
-		for (c = 0; s[c] != '\0'; c++)
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
 		{
-			if (s[c] == ' ')
-				flag = 0;
-			else if (flag == 0)
-			{
-				flag = 1;
-				w++;
-			}
+			flag = 1;
+			w++;
 		}
+	}
 
-		return (w);
+	return (w);
 }
 
 /**
