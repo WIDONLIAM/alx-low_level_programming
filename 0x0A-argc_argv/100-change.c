c1bf2dd12a9db07c4ec64553cbf12ca9c1a079a3
widonliam <wildonliam47@gmail.com>
2023-08-09 18:04:46 -0700

    Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,8 +7,9 @@
  * @argc: Is the number of arguments
  * @argv: Is the array of arguments
  *
- * Return: 0 (Success), 1 (Error)
+ * Return: (0) (Success), (1) (Error)
  */
+
 int main(int argc, char *argv[])
 {
 	int coins[] = {25, 10, 5, 2, 1};
